4ac717ef8780f9aa7942de7e41eb2cb31d3cd103
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2021-10-18 14:58:45 +0200

    Some items were missing contents file
    Because the video was not found; because galerie has zeropadded numbers
    in filename

--- NFA/scripts/run.sh
+++ NFA/scripts/run.sh
@@ -42,8 +42,12 @@ function sanitycheck {
 
 function addFiles {
     local MOV_DIR=$1
-    local SHOT_NO=$2
-    local ID=$3
+    # 3901034-06
+    local ID=$2
+    # This is zero padded as it comes from the identifier that padds the part after '-' to two digits
+    local PADDED_SHOT_NO=${ID#*-}
+    local NOT_PADDED_SHOT_NO
+    NOT_PADDED_SHOT_NO=$(printf "%g\n" "$PADDED_SHOT_NO")
 
     local f
     local suffix
@@ -52,7 +56,7 @@ function addFiles {
     for suffix in mov mp4; do
         # Mnichov: ZF_0340_3900403_sot3_hd-master.mov
         # Galerie: 3901034_26_hd-master.mov
-        find $MOV_DIR/ -type f \( -name "*sot${SHOT_NO}*.$suffix" -o -name "*_${SHOT_NO}_*.$suffix" \) | while read -r f; do
+        find $MOV_DIR/ -type f \( -name "*sot${NOT_PADDED_SHOT_NO}*.$suffix" -o -name "*_${PADDED_SHOT_NO}_*.$suffix" \) | while read -r f; do
             if [ "FALSE" = "$MD_ONLY" ]; then
                 cp $f $ID/
             else
@@ -80,14 +84,13 @@ function output2import {
         local IDENTIFIER=$(xmllint --xpath "/root/item[$i]/dublin_core[@schema='dc']/dcvalue[@element='identifier' and @qualifier='uri']/text()" output.xml)
         local ID=$(basename $IDENTIFIER)
         local HDL=$(basename $(dirname $IDENTIFIER))/$ID
-        local SHOT_NO=${ID##*-*(0)}
         mkdir -p $ID
         xmllint --xpath "/root/item[$i]/dublin_core[@schema='dc']" output.xml | xmllint --encode UTF-8 --format - > $ID/dublin_core.xml
         local schema
         for schema in `xmllint --xpath '/root/item/dublin_core[@schema!="dc"]/@schema' output.xml | sed -e 's#schema=#\n#g' -e's#"##g' | tr -d ' ' | grep . |  sort -u`; do
             xmllint --xpath "/root/item[$i]/dublin_core[@schema='$schema']" output.xml| xmllint --encode UTF-8 --format - > $ID/metadata_$schema.xml
         done
-        addFiles $MOV_DIR $SHOT_NO $ID
+        addFiles $MOV_DIR $ID
         echo "$HDL" > $ID/handle
         # todo license.txt?
     done
